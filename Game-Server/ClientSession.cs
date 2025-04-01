8bdbbae24fbc886abaa9891456143318c444535c
조용필 <78397393+bbie-6772@users.noreply.github.com>
2025-04-01 17:34:37 +0900

    feat: 기본 서버 오픈

---
+++ Game-Server/ClientSession.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+using Server_Core;
+using System.Net;
+
+namespace Game_Server
+{
+	class ClientSession : PacketSession
+	{
+		public int SessionId { get; set; }
+
+		public override void OnConnected(EndPoint endPoint)
+		{
+			Console.WriteLine($"OnConnected : {endPoint}");
+
+		}
+
+		public override void OnRecvPacket(ArraySegment<byte> buffer)
+		{
+			//PacketManager.Instance.OnRecvPacket(this, buffer);
+		}
+
+		public override void OnDisconnected(EndPoint endPoint)
+		{
+			SessionManager.Instance.Remove(this);
+
+			Console.WriteLine($"OnDisconnected : {endPoint}");
+		}
+
+		public override void OnSend(int numOfBytes)
+		{
+			//Console.WriteLine($"Transferred bytes: {numOfBytes}");
+		}
+	}
+}
--- Game-Server/Program.cs
+++ Game-Server/Program.cs
@@ -1,10 +1,32 @@
-﻿namespace Game_Server
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Server_Core;
+
+namespace Game_Server
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
+	class Program
+	{
+		static Listener _listener = new Listener();
+
+		static void Main(string[] args)
+		{
+			// DNS (Domain Name System)
+			string host = Dns.GetHostName();
+			IPHostEntry ipHost = Dns.GetHostEntry(host);
+			IPAddress ipAddr = ipHost.AddressList[0];
+			IPEndPoint endPoint = new IPEndPoint(ipAddr, 5555);
+
+			_listener.Init(endPoint, () => { return SessionManager.Instance.Generate(); });
+			Console.WriteLine($"{endPoint}에서 Listening...");
+
+			while (true)
+			{
+			}
+		}
+	}
 }
