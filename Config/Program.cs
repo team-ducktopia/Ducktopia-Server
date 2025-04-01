1b7fe240022c5ce9fe16cb6dab69e26b11a7e8ec
조용필 <78397393+bbie-6772@users.noreply.github.com>
2025-04-01 16:25:55 +0900

    style: 주석추가

--- Config/Program.cs
+++ Config/Program.cs
@@ -13,20 +13,21 @@ namespace Config
 		}
 
 		public static IHostBuilder CreateHostBuilder(string[] args) =>
-				Host.CreateDefaultBuilder(args)
-						.ConfigureServices((hostContext, services) =>
-						{
-							// 설정 바인딩  
-							services.Configure<AppSettings>(
-									hostContext.Configuration.GetSection("AppSettings")
-							);
+				Host.CreateDefaultBuilder(args)  // 기본 호스트 빌더 생성  
+				.ConfigureServices((hostContext, services) =>  // 서비스 구성  
+				{
+					// 설정 바인딩: JSON의 "AppSettings" 섹션을 AppSettings 클래스에 매핑  
+					services.Configure<AppSettings>(
+					hostContext.Configuration.GetSection("AppSettings")
+			);
 
-							// 서비스 등록  
-							services.AddSingleton(sp =>
-							{
-								var options = sp.GetRequiredService<IOptions<AppSettings>>();
-								return options.Value;
-							});
-						});
+					// AppSettings를 싱글톤으로 직접 등록  
+					services.AddSingleton(sp =>
+					{
+						// IOptions<AppSettings>에서 실제 AppSettings 값 추출  
+						var options = sp.GetRequiredService<IOptions<AppSettings>>();
+						return options.Value;
+					});
+				});
 	}
 }
