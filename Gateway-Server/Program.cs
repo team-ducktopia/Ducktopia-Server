baa40a1de8645561b706c9481cc1566a5f8b2c71
조용필 <78397393+bbie-6772@users.noreply.github.com>
2025-04-01 15:23:52 +0900

    초기 커밋.

---
+++ Gateway-Server/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Gateway_Server
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
